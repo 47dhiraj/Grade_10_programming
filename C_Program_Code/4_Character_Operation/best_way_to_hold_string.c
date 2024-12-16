216de36ac7ce5de2b899d2d3e5320bc1b4061b0e
Dhiraj Kafle <dhirajkafle19@gmail.com>
2024-12-16 17:17:08 +0530

    updated

--- C_Program_Code/4_Character_Operation/best_way_to_hold_string.c
+++ C_Program_Code/4_Character_Operation/best_way_to_hold_string.c
@@ -1,27 +1,26 @@
 
-// Best way & multiple ways to hold string in C program
+// Best way to hold(read) user entered string in C program
 
 #include <stdio.h>
 
 int main()
 {
+	
 	char name[100];
 	
 	printf("Enter your fullname: ");
 	
-	// Best way to read string input
-	fgets(name, sizeof(name), stdin);
 	
-// // Alternative; It is used to read string input (i.e multiple words)
-//	gets(name);
+	// Best Way: It is used to read string input (i.e multiple words)
+	gets(name);
 	
-// // It is used to read single word input
-//	scanf("%s", name);
 	
-	printf("\n%s\n", name);
-
-// // It is used to output string
-//	puts(name);
+	// // It is used to read single word input(i.e won't hold characters/alphabets after space)
+	//	scanf("%s", name);
+	
+	
+	
+	//	printf("\n%s\n", name);
 	
 	return 0;
 }
--- C_Program_Code/4_Character_Operation/firstName-and-lastName-from-fullName.c
+++ C_Program_Code/4_Character_Operation/firstName-and-lastName-from-fullName.c
@@ -1,27 +1,32 @@
-// WAP in C to ask user to input his/her full name & display first name and last name seperate lines.
+// WAP in C to ask user to input his/her full name & display.
 
 #include <stdio.h>
 
 void main() {
 	
-	// Best way to hold fullname or string(multiple words having space):
+	
+	// Best way
+	
 	char fullName[100];
 
     printf("Enter fullName: ");
-    // fgets(fullName, sizeof(fullName), stdin);
+    
 	gets(fullName);
 
     printf("\n %s \n", fullName);
 
+	
 
 //	// Alternative Way 
 //    char firstName[50], middleName[50], lastName[50];
 //
 //    printf("Enter your fullName (firstName middleName lastName): ");
-//    // Note: Both space agi & pachi ko word/characters lai accept(hold) garna lai "%s %s" (i.e bich ma space dine)
-//    scanf("%s %s %s", firstName, lastName, middleName);
 //    
-//    // Displaying in Same line
+//    scanf("%s %s %s", firstName, middleName, lastName);
+//    
 //    printf("\n%s %s %s\n", firstName, middleName, lastName);
+    
+    
+    getch();
     
 }
